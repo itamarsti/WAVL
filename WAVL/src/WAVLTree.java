00d426f81ea668ad3ef8a9f825e0308e2ba7d38b
itamarsti <itamarsti@gmail.com>
2016-12-15 21:12:36 +0200

    new changes

--- WAVL/src/WAVLTree.java
+++ WAVL/src/WAVLTree.java
@@ -11,8 +11,6 @@
 public class WAVLTree {
 	
 	WAVLNode root;
-	int height;
-	int depth;
 	WAVLNode minode;
 	WAVLNode maxnode;
 	int size = 0;
@@ -79,33 +77,53 @@ public class WAVLTree {
 	   //initialize 
 	   WAVLNode cur = this.root; 
 	   WAVLNode add = new WAVLNode(k,i);
+	   add.rank = 0;
+	   if(this.empty()){						//in case the tree is empty (setting root)
+		   cur = add;
+		   this.minode = add;
+		   this.maxnode = add;
+		   this.size++;
+		   return 0;  
+	   }
+	   else{
+		   if(add.key< this.minode.key){
+			   this.minode = add;
+		   }
+		   else if (add.key>this.maxnode.key){
+			   this.maxnode = add;
+		   }
+		}
+	   
 	   while(cur!=null){          
 			if (cur.key == k){           //if key is in tree
 				return -1;
 			}
 			else if(k<cur.key){
+				WAVLNode tmp = cur;
 				cur = cur.left;
 				if (cur == null){        //place to be added found
 					cur = add;
-					add.parent = cur.parent;
-					return rebalance(add,0);         //****need to work on****
+					add.parent = tmp;
+					return rebalance(add);         //****need to work on****
 				}
 				else{
 					continue;			//continue to the next loop
 				}
 			}
 			else{
+				WAVLNode tmp = cur;
 				cur = cur.right;
 				if (cur == null){        //place to be added found
 					cur.right = add;
-					add.parent = cur.parent;
-					return rebalance(add,0);     //****need to work on****
+					add.parent = tmp;
+					return rebalance(add);     //****need to work on****
 				}
 				else{
 					continue;
 				}
 			}
 		}
+	   return 0; //should not reach here
  	}
 
  
@@ -139,7 +157,7 @@ public class WAVLTree {
 				}
 				else if(next.key == k){
 					cur.left = null;
-					return rebalance(cur,0);
+					return rebalance(cur);
 				}
 				else{
 					cur = next;
@@ -153,7 +171,7 @@ public class WAVLTree {
 				}
 				else if(next.key == k){
 					cur.right = null;
-					return rebalance(cur,0);
+					return rebalance(cur);
 				}
 				else{
 					cur = next;
@@ -163,21 +181,151 @@ public class WAVLTree {
 		//if tree is empty
 		return -1;
  }
+ //----------------------------------------------Rebalance-------------------------------
  
- private int rebalance(WAVLNode node,int cnt){
-	   WAVLNode parent = node.parent;
-	   int parank = parent.rank;
-	   if (parank == 0){
-		   parent.rank = 1;
-		   cnt += 1;
-//		   this.root.rebalance(parent);
-	   }
+ 	private int rebalance(WAVLNode node){
+ 		node.rank = 0; //can delete later if new node default rank is 0
+ 		int cnt = 0;
+ 		WAVLNode parent = node.parent;
+ 		int pa_rank = parent.rank;
+ 		if (pa_rank==1){							//if Case B
+ 			return cnt;
+ 			}
+ 		else{										//if Case A: both node.parent kids were null
+ 			pa_rank ++;
+ 			//if case 1....
+ 			cnt = rebalance_rec(parent, 0); //recursive rebalancing calls (calls helper func)
+ 			//if case 2
+ 		}
+ 		
+   return cnt;
 	   
-	   return cnt;
-	   
- }
-
-
+ } //end of main rebalance func
+ 	
+ 	
+ 	private int rebalance_rec(WAVLNode node, int cnt){
+ 		if (node == this.root){ //end condition : reached root
+ 			return cnt;
+ 		}
+ 		int check = Case(node); //check which case needs to be handled (calls helper func)
+ 		switch (check){
+ 			case 0:
+ 				return cnt;
+ 			case 1:
+ 				node.parent.rank ++;				//promote parent and continue rebalancing
+ 				cnt++;
+ 				rebalance_rec(node.parent, cnt);		
+ 			case 2:
+ 				rotate(node,false);
+ 				node.right.rank -- ;
+ 				return cnt + 2;
+ 			case 3:
+ 				rotate(node.right,true);
+ 				rotate(node.parent,false);
+ 				node.rank --;
+ 				node.parent.rank ++ ;
+ 				node.parent.right.rank -- ;
+ 				return cnt + 5;
+ 			case 5:
+ 				rotate(node,true);
+ 				node.left.rank --;
+ 				return cnt+2;
+ 			case 6:	
+ 				rotate(node.left,false);
+ 				rotate(node.parent,true);
+ 				node.rank -- ;
+ 				node.parent.rank ++ ;
+ 				node.parent.left.rank -- ;
+ 				return cnt + 5; 
+ 		}
+ 		return 0;	
+ 	}//end of recursive rebalance helper func
+ 		
+ 	
+ 	private int Case(WAVLNode node){
+ 		WAVLNode parent = node.parent;
+ 		if (RankLeft(parent)==0 && RankRight(parent)==1){
+ 			return 1;
+ 		}
+ 		else if (RankLeft(parent)==1 && RankRight(parent)==0){ //mirror image of case 1
+ 			return 1; 
+ 		}
+ 		else { // final rebalances
+ 			if (RankRight(parent)==2){
+ 				if (RankRight(node) == 2){
+ 					return 2;
+ 				}
+ 				else if(RankLeft(node) == 2){
+ 					return 3;
+ 				}
+ 			}
+ 			else if (RankLeft(parent)==2){ //mirror images
+ 				if (RankRight(node) == 2){
+ 					return 6; // mirror image of 3
+ 				}
+ 				else if(RankLeft(node) == 2){
+ 					return 5; //mirror image of 2
+ 				}
+ 			}	
+ 		}
+ 		return 0 ; //tree is legal (parent had rank diff of 2 before rebalancing)
+ 	}// end of case checker helper func
+ 	
+ 	private void rotate(WAVLNode node,boolean left){
+ 		//----------------------------------------//
+ 		WAVLNode bTemp = node.right;
+ 		if (left){						
+ 			 bTemp = node.left;
+ 		}
+ 		//----------------------------------------//
+		WAVLNode zParent = node.parent;
+		WAVLNode grandParent = node.parent.parent;
+		//connect x parent to be x right child
+		if (left){
+			node.left = zParent;
+		}
+		else{
+			node.right = zParent;
+		}
+		//connect grandparent of x to x
+		if(grandParent.left == zParent){  //if z is left child
+			grandParent.left = node;
+		}
+		else {
+			grandParent.right = node;
+		}
+		//connect x to its grandparent
+		node.parent = grandParent;
+		//connect x new child to x
+		zParent.parent = node;
+		//connect x child to grandson
+		if (left){
+			zParent.right = bTemp;
+		}
+		else{
+			zParent.left = bTemp;
+		}
+		//connect x grandson to child
+		bTemp.parent = zParent; 
+ 	}//end of rotate right helper func
+ 	
+ 	
+	
+	private int RankRight(WAVLNode node){ //input is parent node (no leaves)
+		if (node.right == null){ //is a "-1" node
+			return 2;
+		}
+		return Math.abs(node.rank-node.right.rank);
+	}//end of right rank checker helper func
+	
+	private int RankLeft(WAVLNode node){ //input is parent node (no leaves)
+		if (node.left == null){ //is a "-1" node
+			return 2; 
+		}
+		return Math.abs(node.rank-node.left.rank);
+	}// end of left rank checker helper func
+		
+ 	
    /**
     * public String min()
     *
@@ -274,7 +422,7 @@ public class WAVLTree {
   public class WAVLNode{
 	  private int key;
 	  String info;
-	  int rank;
+	  int rank = 0;
 	  WAVLNode right;
 	  WAVLNode left;
 	  WAVLNode parent;
